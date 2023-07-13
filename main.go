40eaa27983d412131d031251b02ed7fe10c945d8
Asep Teguh Hidayat <114033670+Aguh18@users.noreply.github.com>
2023-07-13 08:12:53 +0700

    init

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	_ "database/sql" // add this
+	"fmt"
+	"log"
+	"net/http"
+	"orm/auth"
+	"orm/middleware"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	
+	"github.com/jinzhu/gorm"
+	
+	_ "github.com/lib/pq" // add this
+)
+
+type newStudent struct {
+	Student_id       uint64 `json:"student_id" binding:"required"`
+	Student_name     string `json:"student_name" binding:"required"`
+	Student_age      uint64 `json:"student_age" binding:"required"`
+	Student_address  string `json:"student_address" binding:"required"`
+	Student_phone_no string `json:"student_phone_no" binding:"required"`
+}
+
+func postHandler(c *gin.Context, db *gorm.DB) {
+	var newStudent newStudent
+	c.Bind(&newStudent)
+	db.Create(&newStudent)
+	c.JSON(http.StatusOK, gin.H{"message": "success create", "data": newStudent})
+}
+
+func getAllHandler(c *gin.Context, db *gorm.DB) {
+	var newStudent []newStudent
+
+	db.Find(&newStudent)
+	c.JSON(http.StatusOK, gin.H{"message": "success find all", "data": newStudent})
+
+}
+
+func getHandler(c *gin.Context, db *gorm.DB) {
+	var newStudent newStudent
+	studentId := c.Param("student_id")
+
+	if db.Find(&newStudent, "student_id=?", studentId).RecordNotFound() {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "data not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "success find by id", "data": newStudent})
+
+}
+
+func putHandler(c *gin.Context, db *gorm.DB) {
+	var newStudent = newStudent{}
+
+	studentId := c.Param("student_id")
+
+	if db.Find(&newStudent, "student_id=?", studentId).RecordNotFound() {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "not found",
+		})
+		return
+	}
+
+	var reqStudent = newStudent
+
+	c.Bind(&reqStudent)
+
+	db.Model(&newStudent).Where("student_id=?", studentId).Update(reqStudent)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success update",
+		"data":    reqStudent,
+	})
+
+}
+
+func delHandler(c *gin.Context, db *gorm.DB) {
+	var newStudent newStudent
+
+	studentId := c.Param("student_id")
+
+	db.Delete(&newStudent, "student_id=?", studentId)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success delete",
+	})
+
+}
+
+func setupRouter() *gin.Engine {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	conn := "postgresql://aguh:7ermaniS.@127.0.0.1/guhh?sslmode=disable"
+	db, err := gorm.Open("postgres", conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Migrate(db)
+
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "succes",
+		})
+	})
+	r.POST("/login", auth.LoginHandler)
+
+	r.POST("/student", func(ctx *gin.Context) {
+		postHandler(ctx, db)
+	})
+
+	r.GET("/student", middleware.Authvalid, func(ctx *gin.Context) {
+		getAllHandler(ctx, db)
+	})
+
+	r.GET("/student/:student_id", middleware.Authvalid, func(ctx *gin.Context) {
+		getHandler(ctx, db)
+	})
+
+	r.PUT("/student/:student_id", middleware.Authvalid, func(ctx *gin.Context) {
+		putHandler(ctx, db)
+	})
+
+	r.DELETE("/student/:student_id", middleware.Authvalid, func(ctx *gin.Context) {
+		delHandler(ctx, db)
+	})
+
+	return r
+
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&newStudent{})
+
+	data := newStudent{}
+	if db.Find(&data).RecordNotFound() {
+		fmt.Println("=================== run seeder user ======================")
+		seederUser(db)
+	}
+}
+
+func seederUser(db *gorm.DB) {
+	data := newStudent{
+		Student_id:       1,
+		Student_name:     "Dono",
+		Student_age:      20,
+		Student_address:  "Jakarta",
+		Student_phone_no: "0123456789",
+	}
+
+	db.Create(&data)
+}
+
+func main() {
+	r := setupRouter()
+
+	r.Run(":8080")
+
+}
